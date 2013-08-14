8dffe49fbcc8819620d81cc7e4c6fae095ae1a01
NicolasBadey <nicolas.badey@gmail.com>
2013-08-14 17:24:26 +0200

    add parameters in DI

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -61,6 +61,10 @@ class Configuration implements ConfigurationInterface
                         ->booleanNode('save_referer_path')->defaultTrue()->end()
                     ->end()
                 ->end()
+                ->scalarNode('mobile_detector_class')->defaultValue('SunCat\MobileDetectBundle\DeviceDetector\MobileDetector')->cannotBeEmpty()->end()
+                ->scalarNode('device_view_class')->defaultValue('SunCat\MobileDetectBundle\Helper\DeviceView')->cannotBeEmpty()->end()
+                ->scalarNode('request_listener_class')->defaultValue('SunCat\MobileDetectBundle\EventListener\RequestListener')->cannotBeEmpty()->end()
+                ->scalarNode('extension_class')->defaultValue('SunCat\MobileDetectBundle\Twig\Extension\MobileDetectExtension')->cannotBeEmpty()->end()
             ->end();
 
         return $treeBuilder;
