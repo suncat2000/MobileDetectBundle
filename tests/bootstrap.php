9bcf1b518d340c1edfdce6b5e3ac4f16675d059c
Théo Attali <theoattali@gmail.com>
2022-03-30 19:16:57 -0300

    use only pcov

--- tests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-require dirname(__DIR__).'/vendor/autoload.php';
-
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-}
